0af8dde0adfa27af93330007d17aae9f4254ab11
polarfox42 <vg.mkrtchyan@gmail.com>
2019-08-12 19:01:29 +0500

    update parser

--- make_file_list.py
+++ make_file_list.py
@@ -7,40 +7,54 @@ import codecs
 
 def create_parser():
     '''Создаем и настраиваем парсер'''
-    parser = argparse.ArgumentParser()
-    parser.add_argument('directory_to_describe', nargs='?')
-    parser.add_argument('directory_to_save', nargs='?')
+    parser = argparse.ArgumentParser(
+        description = '''Программа для составления списка текстовых файлов 
+        (.txt, .csv) в указанной директории''',
+        add_help = False
+    )
+    args_group = parser.add_argument_group(title = 'Аргументы')
+    options_group = args_group = parser.add_argument_group(title = 'Параметры')
+
+    args_group.add_argument('describe', nargs='?',
+                        help = 'Адрес директории для составления списка файлов')
+    args_group.add_argument('save', nargs='?',
+                        help = 'Адрес директории для сохранения файла со списком')
+    options_group.add_argument('-c', '--coding', nargs='?', default = 'cp1251',
+                        metavar = 'КОДИРОВКА',
+                        help = 'Кодировка исходных файлов (по умолчанию "cp1251")')
+    options_group.add_argument('--help', '-h', action = 'help', help = 'Справка')
 
     return parser
 
 
 def parse_arguments(arguments):
     '''Разбираем аргументы и переназначаем в случае необходимости'''
-    if arguments.directory_to_describe is None:
-        arguments.directory_to_describe = os.getcwd()
-    if arguments.directory_to_save is None:
-        arguments.directory_to_save = arguments.directory_to_describe
+    if arguments.describe is None:
+        arguments.describe = os.getcwd()
+    if arguments.save is None:
+        arguments.save = arguments.describe
 
     return arguments
 
 
-def get_first_line(file_path):
+def get_first_line(file_path, coding):
     '''Добываем из файла первую строку'''
-    file = codecs.open(file_path, 'r', 'cp1251')
+    file = codecs.open(file_path, 'r', coding)
     first_line = file.readline()
 
     return first_line
 
 
-def create_file_list(directory_path):
+def create_file_list(directory_path, coding):
     '''Создаем список файлов в данной директории'''
     file_list = os.listdir(directory_path)
     data_for_export = []
+    knowing_types = ['txt', 'csv']
     # собираем нужные данные
     for file in file_list:
         path = os.path.join(directory_path, file)
-        if os.path.isfile(path) and path[-3:] == 'txt':
-            first_line = get_first_line(path)
+        if os.path.isfile(path) and path[-3:] in knowing_types:
+            first_line = get_first_line(path, coding)
             data_for_export.append((file, first_line))
 
     return data_for_export
@@ -61,8 +75,8 @@ def main() -> None:
     parser = create_parser()
     namespace = parser.parse_args()
     namespace = parse_arguments(namespace)
-    file_list = create_file_list(namespace.directory_to_describe)
-    create_file(file_list, namespace.directory_to_save)
+    file_list = create_file_list(namespace.describe, namespace.coding)
+    create_file(file_list, namespace.save)
 
 
 if __name__ == '__main__':
